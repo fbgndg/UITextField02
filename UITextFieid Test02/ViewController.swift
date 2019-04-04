2944d297007c811f414969699d1f5f4a60f641c7
D7703_19 <d7703_19@d7703-19ui-imac.local>
2019-04-04 16:52:08 +0900

    s

--- UITextFieid Test02/ViewController.swift
+++ UITextFieid Test02/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        print(myTextField.text!)
+        let inString = myTextField.text
+        let outString = inString! + "Coding"
+        myLabel.text = outString
+        myTextField.text = "" //다음을 위하여 공백
+        
+        //키보드를 강제적으로 내림
+        myTextField.resignFirstResponder()
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        myTextField.backgroundColor = UIColor.yellow
+        view.backgroundColor = UIColor.green
+    }
 }
 
